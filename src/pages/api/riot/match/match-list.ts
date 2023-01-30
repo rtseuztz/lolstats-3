b645656aae4848709e0c8b27f00de6c9e7efbf21
Saji Shunnarah <sajishunnarah@gmail.com>
2023-01-30 17:11:17 -0500

    fully fixed types and rate limiter. made func abstract added other routes

--- src/pages/api/riot/match/match-list.ts
+++ src/pages/api/riot/match/match-list.ts
@@ -7,9 +7,17 @@ import { buildQuery, err } from "../riot/RiotFunctions";
  */
 export default class match extends Riot {
     query = `https://{REGION}.api.riotgames.com/lol/match/v5/matches/by-puuid/{0}/ids?start=0&count={1}`
-    readonly functionAdder = matchListLimiter.addFunction;
+    readonly promiseRateLimiter = matchListLimiter;
     constructor(values: string[], region: string) {
         super(values, region)
         this.query = buildQuery(this.query, this.values, this.region);
+    }
+    async execute(): Promise<[number[], err | null]> {
+        var [res, err] = await super.execute()
+        if (err) {
+            return [[], err]
+        } else {
+            return [res as number[], err]
+        }
     }
 }
